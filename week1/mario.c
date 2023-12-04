76b16197a1265384b1c068b0e26d0d3a4bdf65dd
Soll✯ <48865246+sarasoll@users.noreply.github.com>
2023-12-04 12:35:30 +0300

    mario.c

--- week1/mario.c
+++ week1/mario.c
@@ -1,32 +1,29 @@
-
 #include <cs50.h>
 #include <stdio.h>
 
-int get_size(void);
-void print_grid(int n);
-
 int main(void)
 {
-    int n = get_size();
-    print_grid(n);
-}
-
-int get_size(void)
-{
-    int n;
+    int h;
     do
     {
-        n = get_int("Size: ");
+        h = get_int("Height: ");
     }
-    while (n < 1);
-    return n;
-}
+    while (h < 1 || h > 8);
 
-void print_grid(int n)
-{
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= h; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = h - i; j > 0; j--)
+        {
+            printf(" ");
+        }
+
+        for (int k = 0; k < i; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+
+        for (int k = 0; k < i; k++)
         {
             printf("#");
         }
